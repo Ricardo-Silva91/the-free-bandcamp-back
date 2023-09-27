7de8dbd72a745bae16851c386ea622cc2fd7534e
Ricardo Silva <ricardos8977@gmail.com>
2023-09-27 09:24:46 +0100

    print row count

--- src/scripts/cleanDatabase.js
+++ src/scripts/cleanDatabase.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const { getRows, getDoc } = require('../utils/google.utils');
+const { getRows, getDoc, getRowCount } = require('../utils/google.utils');
 const { getDetailsForAllAlbums } = require('../utils/browser.utils');
 
 const cleanDatabase = async () => {
   const doc = await getDoc();
+  const rowCount = getRowCount(doc);
   
-  console.log('cleaning');
+  console.log('cleaning', { rowCount });
 };
 
 cleanDatabase();
